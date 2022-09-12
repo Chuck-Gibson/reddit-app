70aa0762887ff4a27da4a440fd192845589a7716
Hal <hn.nguyen275@gmail.com>
2022-09-11 17:07:12 -0700

    added logo svg

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './header.css';
-import Searchbar from '../searchbar/searchBar';
+import React from "react";
+import "./header.css";
+import Searchbar from "../searchbar/searchBar";
+import { ReactComponent as Logo } from "assets/logo.svg";
 
 function Header() {
-    return(
-        <div className='Header'>
-            <img src='./redditlogo.png' id="logo" alt='logo'></img>
-            <Searchbar/>
-            <button id="sign-in">Sign in</button>
-        </div>
-    )
-};
+  return (
+    <div className="Header">
+      <div class="app-logo">
+        <Logo />
+      </div>
+      <Searchbar />
+      <button id="sign-in">Sign in</button>
+    </div>
+  );
+}
 
 export default Header;
