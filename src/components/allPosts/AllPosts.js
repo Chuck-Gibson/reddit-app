7eb3fba4200d734d3a10d4b28b8186c330ff8d74
Hal <haln_01@proton.me>
2022-09-16 00:51:14 -0700

    building post component

--- src/components/allPosts/AllPosts.js
+++ src/components/allPosts/AllPosts.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getPosts } from "store/slices/userPosts/asyncThunks/getPosts";
+import { AllPostsContainer } from "./AllPostsContainer";
 import "./allPosts.css";
 
 const AllPosts = () => {
+  // Fetch post
   const dispatch = useDispatch();
   useEffect(() => {
-    // dispatch(getPosts("hot")); // get all post on mount
+    dispatch(getPosts("rising")); // get all post on mount
   });
 
   return (
     <div className="AllPosts">
-      BRB gotta figure out how to attach the comments of each post to the json
-      you are getting back
+      <AllPostsContainer />
     </div>
   );
 };
