a45827c1fa6cd3f107912c093fa7a1c0a32cd796
Chuck Gibson <90487306+Chuck-Gibson@users.noreply.github.com>
2022-09-19 22:49:57 -0600

    added load circle, cleaned up trending

--- src/components/trending/trending.js
+++ src/components/trending/trending.js
@@ -13,26 +13,31 @@ const Trending = () => {
 
   const isLoading = useSelector(selectTrendingPosts).status;
   const trendingPosts = useSelector(selectTrendingPosts).posts;
-  // console.log(trendingPosts);
- if (isLoading === "pending") {
-    return <div> Loading trending</div>;
+
+
+  if (isLoading === "pending") {
+    return <div className="loader"></div>;
   } 
   else {
 
-  return (trendingPosts.map((post) => {
+  return (
+    <div data-testid="component--trending" id="Trending">
+    <h2>Trending</h2>
+    {trendingPosts.map((post) => {
     return(
-    <div className="Trending">
+    <div className="Trending-post-container">
       {trendingPosts.length > 0 && (
-        <TrendingPost
+        <a href={post.url}><TrendingPost
           title={post.title}
-          content={post.media === true && post.media.contentUrl}
+          content={post.media === true? post.media.contentUrl : post.media}
           subreddit={post.subreddit}
           url={post.url}
-        ></TrendingPost>
+        ></TrendingPost></a>
       )}
     </div>
     )
-  }));
+  })}</div>
+  );
 };
 };
 
